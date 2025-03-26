3061fba500ee2e452c0cfd0ff0c89d92bf82c9d2
“['Shueb <mohitsainisaini2680@gmail.com>
2025-03-26 12:09:35 +0530

    change url

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 // import User from "../models/userSchema.js";
 
 // const url = "mongodb+srv://mohitsainisaini2680:mohitsaini21@cluster0.wjx3j.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0"nod
-const url = "mongodb://localhost:27017";
+const url =
+  "mongodb+srv://mohitsainisaini2680:mohitsaini21@cluster0.wjx3j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 export const ConnectDB = async () => {
   try {
